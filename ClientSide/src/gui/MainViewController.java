2839a06861d7fe352bc1d10afaf9e055d4b231ef
AlexandreGLopes <alexandre_lopess@hotmail.com>
2021-11-06 17:49:42 -0300

    initial commit on branch alpha

--- ClientSide/src/gui/MainViewController.java
+++ ClientSide/src/gui/MainViewController.java
@@ -1,32 +1,19 @@
 package gui;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.URL;
 import java.util.ResourceBundle;
-import java.util.function.Consumer;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
-import application.Main;
 import gui.util.Alerts;
 import javafx.animation.FadeTransition;
-import javafx.animation.ParallelTransition;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.util.Duration;
