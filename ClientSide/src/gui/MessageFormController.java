5ab859f1e347d02384e1e440812223911b5bec62
AlexandreGLopes <alexandre_lopess@hotmail.com>
2021-11-30 10:18:49 -0300

    jars httpclient e inicio do ManualUpdaterHelp

--- ClientSide/src/gui/MessageFormController.java
+++ ClientSide/src/gui/MessageFormController.java
@@ -70,27 +70,43 @@ public class MessageFormController implements Initializable {
 	// formulário e manda mensagem por whatsapp usando a API que está rodando no
 	// servidor VPS
 	private void sendWhatsAppMessage(ActionEvent event) {
+		// String que vai receber o telefone do objeto que foi passado no controller
 		String telefone = entity.getTelefone();
-
+		// Iniciando o uso do HttpClient da Apache
 		HttpClient httpClient = HttpClientBuilder.create().build();
 
 		try {
+			// Montando um resquest do tipo POST e passando o servidor, a porta e ométodo
+			// usado para mandar mensagem
 			HttpPost request = new HttpPost("http://51.222.146.252:3333/sendText");
-			StringEntity params = new StringEntity("{\"session\":\"terrazzaobscwb\",\"number\":\"" + telefone + "\",\"text\":\""
-					+ textMessage.getText() + "\"}", "UTF-8");
+			// Construindo Body do JSON segundo a API do MyZAP 2.0
+			// Passamos o telefone conforme o Costumer passado e a mensagem pegada do
+			// textArea. Usamos o .replace para formatar toda vez que tiver uma quebra de
+			// linha "\n" adicionar uma barra a mais. Se não ela vai se tornar uma nova
+			// linha no JSON (e não um dado que é passado como String no JSON).
+			StringEntity params = new StringEntity("{\"session\":\"terrazzaobscwb\",\"number\":\"" + telefone
+					+ "\",\"text\":\"" + textMessage.getText().replace("\n", "\\n") + "\"}", "UTF-8");
+			// Headers do JSON segundo a API do MyZAP 2.0
 			request.setHeader("Accept", "application/json");
 			request.setHeader("Content-Type", "application/json");
 			request.setHeader("sessionkey", "terrazza@A8K9hD");
+			// Passando o Body para a requisição
 			request.setEntity(params);
+			// Executando a resquisição e pegando as resposta e colocando na variável
+			// response
 			HttpResponse response = httpClient.execute(request);
+			// Passando o código da resposta da conecção http para uma variável ainda mais
+			// restrita que vai ter apenas o integer do código
 			int statusCode = response.getStatusLine().getStatusCode();
+			// Verificando se o código está dentro da faixa de conclusão bem sucedida
+			// Dependendo do código o Alert é montado para mostrar se deu certo ou não
 			if (statusCode >= 200 && statusCode < 300) {
 				Alerts.showAlert("Mensagem enviada com sucesso!", null, "Sua mensagem foi enviada com sucesso!",
 						AlertType.INFORMATION);
-			}
-			else {
+			} else {
 				Alerts.showAlert("Erro ao enviar mensagem!", null,
-						"Houve um erro ao tentar enviar a mensagem.\nContate o desenvolvedor para saber mais.", AlertType.ERROR);
+						"Houve um erro ao tentar enviar a mensagem.\nContate o desenvolvedor para saber mais.",
+						AlertType.ERROR);
 			}
 		} catch (Exception e) {
 			Alerts.showAlert("Erro ao enviar mensagem!", null,
